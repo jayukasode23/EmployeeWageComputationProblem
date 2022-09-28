43c12eb79cddb6570ff271c6048f75eb11aeecbe
jayukasode23 <jayukasode2398@gmail.com>
2022-09-28 11:38:48 +0530

    Added Greeting Messege

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeWageComputationProblem;
+
+Console.WriteLine("Welcome To EmployeeWage Program");
+
+EmployeeCheck.CheckEmployeeAttendance();
+
+DailyEmployeeWage.CheckDailyWage();
+
+PartTimeEmpWage.EmpWagePartTime();
+
+SwitchCaseStatement.EmployeeWage();
+
+WagesForMonth.MonthlyWage();
+
+CalculateTotalWage.CalculateMonthlyWage();
